7a961cc06dec21e2618d1eb78899af97fc3ab038
Guilherme D. Garcia <guilherme.garcia@mail.mcgill.ca>
2015-09-16 22:00:53 -0400

    Update day2.R

--- McGill_R_Tutorial/day2.R
+++ McGill_R_Tutorial/day2.R
@@ -7,7 +7,12 @@
 #############
 
 
-# Packages: This time, we'll use a couple of packages. Normally, we load all packages together at the top of the script (for practical reasons only). This way, it's easy to see where all packages are. Packages are like modules: each one is a collection of functions (among other things) that we can use. In a way, loading a package is like adding functions to R---this makes R a very powerful tool, since anyone can create a package. It's almost always the case that there's a package for everything you want to do.
+# Packages: This time, we'll use a couple of packages. 
+# Normally, we load all packages together at the top of the script (for practical reasons only). 
+# This way, it's easy to see where all packages are. Packages are like modules: each one is a collection of functions 
+# (among other things) that we can use. In a way, loading a package is like adding functions to R (this makes R a 
+# very powerful tool, since anyone can create a package). 
+# Whenever you want to do something more specific, chances are there's a package for it.
 
 
 library(plyr)
@@ -20,7 +25,9 @@ library(languageR)
 
 setwd("~/Desktop")
 
-# But maybe you want to have a specific folder IN your desktop, something more 'organized'. Let's create a new folder (directory) IN our desktop called 'Day2'. Then, let's set our working directory again, as we'll be working inside 'Day2'.
+# But maybe you want to have a specific folder IN your desktop, something more 'organized'. 
+# Let's create a new folder (directory) IN our desktop called 'Day2'. 
+# Then, let's set our working directory again, as we'll be working inside 'Day2'.
 
 dir.create("Day2")
 
@@ -31,7 +38,8 @@ dir.exists("Day2")
 setwd("~/Desktop/Day2")
 
 
-# Of course, you can create a folder and set your dir manually, using your mouse. But this might be easier/faster. Also, this is a useful way to automate folder creating in your script.
+# Of course, you can create a folder and set your dir manually, using your mouse. 
+# But this might be easier/faster. Also, this is a useful way to automate folder creating in your script.
 
 
 ################################
@@ -56,9 +64,11 @@ data = read.csv("file_name.txt", sep = "\t")
 data = read.csv("file_name.csv")
 
 
-# Even though we'll use data frames most of the time, *data tables* are WAY better (and faster). Their syntax is a bit more intricate, but they're definitely better if you have a lot of data
+# Even though we'll use data frames most of the time, *data tables* are WAY better (and faster). 
+# Their syntax is a bit more intricate, but they're definitely better if you have a lot of data
 
-# In fact, you can calculate how much time it takes to read a file (or do perform any other task) in R. Let's test this loading the Buckeye Corpus:
+# In fact, you can calculate how much time it takes to read a file (or do perform any other task) in R. 
+# Let's test this loading the Buckeye Corpus:
 
 system.time(rnorm(10000000))
 
@@ -79,7 +89,10 @@ system.time(fread(path))
 # We won't be using data.table, but you should know about it—mainly if you work with corpora/large files.
 
 
-# Now, we've loaded our data and the file is stored in a variable called 'data'. Everything we do to this variable will NOT affect the original file. R loaded a 'copy' of tapped.txt, and that's it. If you delete something by mistake, don't worry: the original will always be there as far as you do not export a new txt file and overwrite it (we exported a csv file last time).
+# Now, we've loaded our data and the file is stored in a variable called 'data'. 
+# Everything we do to this variable will NOT affect the original file. R loaded a 'copy' of tapped.txt, 
+# and that's it. If you delete something by mistake, don't worry: the original will always be there as far as
+# you do not export a new txt file and overwrite it (we exported a csv file last time).
 
 # Again, if you want to export the file, you have different ways to do that. Here are two:
 
@@ -97,7 +110,8 @@ write.table(data, "~/Desktop/file_name.txt", sep="\t")
 ############################
 ############################
 
-# Today, we will actually use one of the datasets included in R. More specifically, these data are in an R package called languageR—which, btw, is extremely useful for linguists, of course.
+# Today, we will actually use one of the datasets included in R. More specifically, 
+# these data are in an R package called languageR—which, btw, is extremely useful for linguists, of course.
 
 
 
@@ -130,7 +144,8 @@ head(data)
 
 ?dative
 
-# ? is used to get help on a function OR element in the R language. You can't use it for variables you created, only for terms that have an intrinsic meaning in R (or in one of its packages).
+# ? is used to get help on a function OR element in the R language. You can't use it for variables you created, 
+# only for terms that have an intrinsic meaning in R (or in one of its packages).
 
 
 
@@ -174,7 +189,8 @@ tail(new,10)
 
 names(new) # Column names
 
-# This is particularly useful if you have a large dataset, since it's a very economical way of looking at the variables in the file.
+# This is particularly useful if you have a large dataset, since it's a very economical way of looking at 
+# the variables in the file.
 
 
 # If you want something a little more comprehensive:
@@ -202,7 +218,8 @@ dim(new)
 # Given the number of columns, when you visualize the data, the columns will break.
 
 
-# If you recall, you can also use indices (slice notation) to take a look at the data. Let's say you want to check the first 10 rows of the fourth column:
+# If you recall, you can also use indices (slice notation) to take a look at the data. 
+# Let's say you want to check the first 10 rows of the fourth column:
 
 new[1:10,4] # This will also give you the levels of this factor (if the column is, in fact, a factor)
 
@@ -210,7 +227,8 @@ new[1:10,4] # This will also give you the levels of this factor (if the column i
 
 new$semClass[1:10]
 
-# This says: Inside data, select the column SemanticClass and give me the first 10 items. Note that here you don't use a comma, since a single column only has one dimension.
+# This says: Inside data, select the column SemanticClass and give me the first 10 items. 
+# Note that here you don't use a comma, since a single column only has one dimension.
 
 # SEMANTIC CLASS CODES:  
 # a (abstract: 'give it some thought'), 
@@ -222,7 +240,8 @@ new$semClass[1:10]
 
 
 
-# Another important thing to do right after you load your data is check whether your variables are coded correctly. R will assume numbers are numeric, but sometimes you want numbers to be a factor, for example. This will need to be corrected.
+# Another important thing to do right after you load your data is check whether your variables are coded correctly. 
+# R will assume numbers are numeric, but sometimes you want numbers to be a factor, for example. This will need to be corrected.
 
 
 str(new)
@@ -246,7 +265,8 @@ length(levels(new$verb)) # will give you the number
 ######### Cross-tabulation and proportions
 
 
-# This is a *very* useful way of looking at your data. Even before we plot things, it's nice to take a look at counts/% given certain variables, for example.
+# This is a *very* useful way of looking at your data. Even before we plot things, 
+# it's nice to take a look at counts/% given certain variables, for example.
 
 # Option 1: use xtabs()
 
@@ -278,7 +298,8 @@ xtabs(~realization + semClass, new)
 
 xtabs(~proRec + animacyTheme + semClass, new)
 
-# Note that the order of the arguments does matter. The third argument here is the third dimension, so it's best to pick a factor that has fewer levels.
+# Note that the order of the arguments does matter. The third argument here is the third dimension, 
+# so it's best to pick a factor that has fewer levels.
 
 xtabs(~semClass + animacyTheme + proRec, new)
 
@@ -298,7 +319,8 @@ prop.table(xtabs(~proRec, new)) # * 100 if you wish
 
 prop.table(xtabs(~realization + semClass, new))
 
-# You can see that the default is to calculate %s over *all* cells, which isn't that useful here. We can solve that: For rows, use 1; for cols, use 2.
+# You can see that the default is to calculate %s over *all* cells, which isn't that useful here. 
+# We can solve that: For rows, use 1; for cols, use 2.
 
 prop.table(xtabs(~realization + semClass, new),1)
 
@@ -323,7 +345,8 @@ barplot(counts)
 
 barplot(counts, legend.text = TRUE, args.legend = list(x = "topright"), main = "Pronominal vs. Non-pronominal recipients")
 
-# By saying legend.text = TRUE, we're telling R to add a legend to the plot. This is automatically set, given the structure of counts. However, you might want to add different labels:
+# By saying legend.text = TRUE, we're telling R to add a legend to the plot. This is automatically set, 
+# given the structure of counts. However, you might want to add different labels:
 
 barplot(counts, legend.text = c("Non-pronominal", "Pronominal"), args.legend = list(x = "topright"), main = "Pronominal vs. Non-pronominal recipients")
 
@@ -368,7 +391,8 @@ head(new)
 ######### Summarizing your data
 
 
-# Let's say you want to know the average length of theme by RealizationOfRecipient. Perhaps the longer the theme, the more likely it is to realize the recipient as an NP:
+# Let's say you want to know the average length of theme by RealizationOfRecipient. 
+# Perhaps the longer the theme, the more likely it is to realize the recipient as an NP:
 
 # Give the interesting text to Bob
 
@@ -416,19 +440,23 @@ summ1b
 
 # EXERCISE
 
-# 1. Create a data frame that contains only data for the verb give. Assign it to variable called "give". How many rows does the data frame have?
+# 1. Create a data frame that contains only data for the verb give. Assign it to variable called "give". 
+# How many rows does the data frame have?
 
 give = new[new$verb == "give",]
 
 cat("There are", nrow(give), "rows")
 
-# 2. Summarize that data frame using ddply, adding a column for mean LengthOfTheme. Assign the output to a variable giveSum. You should keep the following columns: animacyRec, realization and semClass. The new column should be called meanLength. Now look at the rows: which case has the largest meanLength of theme?
+# 2. Summarize that data frame using ddply, adding a column for mean LengthOfTheme. Assign the output to a 
+# variable giveSum. You should keep the following columns: animacyRec, realization and semClass. 
+# The new column should be called meanLength. Now look at the rows: which case has the largest meanLength of theme?
 
 giveSum = ddply(give,.(animacyRec, realization, semClass), summarise, meanLength = mean(lengthTheme))
 
 # The highest mean is 5.84: animacyRec = animate, realization = NP, semClass = a.
 
-# 3. What % of cases in give are realized with NP? And PP? Add a second dimension (proRec). Now only look at pronominal cases. What's the % of realization = NP?
+# 3. What % of cases in give are realized with NP? And PP? Add a second dimension (proRec). 
+# Now only look at pronominal cases. What's the % of realization = NP?
 
 prop.table(xtabs(~realization, give))
 
@@ -532,3 +560,4 @@ pdf("plot.pdf")
 hist(result)
 dev.off()
 
+# Of course, you can always simply press cmd + s to save your plot...
